298474a435cce78acf8990780caee3134aaa4691
Xavi Nguyen <vphat28@gmail.com>
2020-12-02 22:49:21 +0700

    3d s2

--- view/frontend/web/js/view/payment/method-renderer/paysafe_gateway.js
+++ view/frontend/web/js/view/payment/method-renderer/paysafe_gateway.js
@@ -5,185 +5,189 @@
 /*browser:true*/
 /*global define*/
 define(
-    [
-        'ko',
-        'jquery',
-        'Magento_Payment/js/view/payment/cc-form',
-        'Magento_Payment/js/model/credit-card-validation/validator',
-        'mage/url',
-        'paysafe3ds2sdk',
-        'Magento_Checkout/js/model/full-screen-loader'
-    ],
-    function (ko, $, Component, validator, urlBuilder, paysafe, fullScreenLoader) {
-        'use strict';
+  [
+    'ko',
+    'jquery',
+    'Magento_Payment/js/view/payment/cc-form',
+    'Magento_Payment/js/model/credit-card-validation/validator',
+    'mage/url',
+    'paysafe3ds2sdk',
+    'Magento_Checkout/js/model/full-screen-loader'
+  ],
+  function (ko, $, Component, validator, urlBuilder, paysafe, fullScreenLoader) {
+    'use strict';
 
-        return Component.extend({
-            defaults: {
-                template: 'Paysafe_Payment/payment/form',
-                transactionResult: '',
-                completedTxnId: '',
-                accordDChoice: '',
-                accordDType: '',
-                accordDGracePeriod: '',
-                accordDPlanNumber: '',
-            },
+    return Component.extend({
+      defaults: {
+        template: 'Paysafe_Payment/payment/form',
+        transactionResult: '',
+        completedTxnId: '',
+        accordDChoice: '',
+        accordDType: '',
+        accordDGracePeriod: '',
+        accordDPlanNumber: '',
+      },
 
-            initObservable: function () {
-                this._super()
-                    .observe([
-                        'accordDChoice',
-                        'accordDType',
-                        'accordDPlanNumber',
-                        'accordDGracePeriod',
-                    ]);
+      initObservable: function () {
+        this._super()
+          .observe([
+            'accordDChoice',
+            'accordDType',
+            'accordDPlanNumber',
+            'accordDGracePeriod',
+          ]);
 
-                return this;
-            },
+        return this;
+      },
 
-            afterPlaceOrder: function () {
-                if (parseInt(window.checkoutConfig.payment.paysafe_gateway.threedsecuremode) === 2) {
-                    return;
-                }
+      afterPlaceOrder: function () {
+        if(parseInt(window.checkoutConfig.payment.paysafe_gateway.threedsecuremode) === 2) {
+          return;
+        }
 
-                window.location.href = urlBuilder.build('paysafe/cc/redirect');
-            },
+        window.location.href = urlBuilder.build('paysafe/cc/redirect');
+      },
 
-            /**
-             * @returns {Boolean}
-             */
-            isShowLegend: function () {
-                return true;
-            },
+      /**
+       * @returns {Boolean}
+       */
+      isShowLegend: function () {
+        return true;
+      },
 
-            getThisObject: function () {
-                return this;
-            },
+      getThisObject: function () {
+        return this;
+      },
 
-            getCode: function () {
-                return 'paysafe_gateway';
-            },
+      getCode: function () {
+        return 'paysafe_gateway';
+      },
 
-            isActive: function () {
-                return window.checkoutConfig.payment.paysafe_gateway.active;
-            },
+      isActive: function () {
+        return window.checkoutConfig.payment.paysafe_gateway.active;
+      },
 
-            isEnableAccordD: function () {
-                return window.checkoutConfig.payment.paysafe_gateway.enable_accordD;
-            },
+      isEnableAccordD: function () {
+        return window.checkoutConfig.payment.paysafe_gateway.enable_accordD;
+      },
 
-            initialize: function () {
-                this._super();
-            },
+      initialize: function () {
+        this._super();
+      },
 
-            initElement: function () {
-                this._super();
-                this.initValidation();
-            },
+      initElement: function () {
+        this._super();
+        this.initValidation();
+      },
 
-            getAccordDTypes: function () {
-                return [
-                    {
-                        'value': '1',
-                        'text': 'Deferred'
+      getAccordDTypes: function () {
+        return [
+          {
+            'value': '1',
+            'text': 'Deferred'
+          },
+          {
+            'value': '2',
+            'text': 'Equal'
+          }
+        ]
+      },
+
+      getTestMode: function () {
+        return window.checkoutConfig.payment.paysafe_gateway.testmode;
+      },
+
+      placeOrderWrap: function () {
+        var self = this;
+        var paysafe3ds = window.paysafe;
+        if($('#co-transparent-form-paysafe').validation('isValid')) {
+          if(parseInt(window.checkoutConfig.payment.paysafe_gateway.threedsecuremode) === 1) {
+            self.redirectAfterPlaceOrder = false;
+
+            return self.placeOrder();
+          }
+
+          // If 3ds version 2
+          if(parseInt(window.checkoutConfig.payment.paysafe_gateway.threedsecuremode) === 2) {
+            fullScreenLoader.startLoader();
+            if(self.getTestMode()) {
+              var environment = 'TEST';
+            }else {
+              var environment = 'LIVE';
+            }
+
+            paysafe3ds.threedsecure.start(window.checkoutConfig.payment.paysafe_gateway.base64apikey, {
+              environment: environment,
+              accountId: window.checkoutConfig.payment.paysafe_gateway.accountid,
+              card: {
+                cardBin: self.creditCardNumber().substring(0, 8)
+              }
+            }, function (deviceFingerprintingId, error) {
+              if(typeof deviceFingerprintingId === 'undefined') {
+                alert(error.detailedMessage);
+                return;
+              }
+              var url = '/paysafe/cc/threedauthentication';
+              var request = $.ajax({
+                url: url,
+                method: "POST",
+                dataType: "json",
+                beforeSend: function (xhr) {
+                  /* Authorization header */
+                  xhr.setRequestHeader("Authorization", "Basic " + window.checkoutConfig.payment.paysafe_gateway.base64apikey);
+                },
+                data: {
+                  "deviceFingerprintingId": deviceFingerprintingId,
+                  "card": {
+                    "cardExpiry": {
+                      "month": self.creditCardExpMonth(),
+                      "year": self.creditCardExpYear()
                     },
-                    {
-                        'value': '2',
-                        'text': 'Equal'
-                    }
-                ]
-            },
-
-            getTestMode: function () {
-                return window.checkoutConfig.payment.paysafe_gateway.testmode;
-            },
-
-            placeOrderWrap: function () {
-                var self = this;
-                var paysafe3ds = window.paysafe;
-                if ($('#co-transparent-form-paysafe').validation('isValid')) {
-                    if (parseInt(window.checkoutConfig.payment.paysafe_gateway.threedsecuremode) === 1) {
-                        self.redirectAfterPlaceOrder = false;
-
-                        return self.placeOrder();
-                    }
-
-                    // If 3ds version 2
-                    if (parseInt(window.checkoutConfig.payment.paysafe_gateway.threedsecuremode) === 2) {
-                        fullScreenLoader.startLoader();
-                        if (self.getTestMode()) {
-                            var environment = 'TEST';
-                        } else {
-                            var environment = 'LIVE';
-                        }
-
-                        paysafe3ds.threedsecure.start(window.checkoutConfig.payment.paysafe_gateway.base64apikey, {
-                            environment: environment,
-                            accountId: window.checkoutConfig.payment.paysafe_gateway.accountid,
-                            card: {
-                                cardBin: self.creditCardNumber().substring(0, 8)
-                            }
-                        }, function (deviceFingerprintingId, error) {
-                            var url = '/paysafe/cc/threedauthentication';
-                            var request = $.ajax({
-                                url: url,
-                                method: "POST",
-                                dataType: "json",
-                                beforeSend: function (xhr) {
-                                    /* Authorization header */
-                                    xhr.setRequestHeader("Authorization", "Basic " + window.checkoutConfig.payment.paysafe_gateway.base64apikey);
-                                },
-                                data: {
-                                    "deviceFingerprintingId": deviceFingerprintingId,
-                                    "card": {
-                                        "cardExpiry": {
-                                            "month": self.creditCardExpMonth(),
-                                            "year": self.creditCardExpYear()
-                                        },
-                                        "cardNum": self.creditCardNumber()
-                                    },
-                                }
-                            })
-                                .done(function (data) {
-                                    data = JSON.parse(data);
-                                    console.log(data);
-                                    if (data.status === 'threed2completed') {
-                                        self.completedTxnId = data.dataLoad.id;
-                                        return self.placeOrder();
-                                    }
-                                });
-                        });
-                    }
-
-                    if (parseInt(window.checkoutConfig.payment.paysafe_gateway.threedsecuremode) === 0) {
-                        return self.placeOrder();
-                    }
+                    "cardNum": self.creditCardNumber()
+                  },
                 }
-            },
-
-            initValidation: function () {
-                $('#co-transparent-form-paysafe').on('keyup  paste', 'input, select, textarea', function () {
-                    $('#co-transparent-form-paysafe').validation('isValid');
+              })
+                .done(function (data) {
+                  data = JSON.parse(data);
+                  console.log(data);
+                  if(data.status === 'threed2completed') {
+                    self.completedTxnId = data.dataLoad.id;
+                    return self.placeOrder();
+                  }
                 });
-            },
+            });
+          }
 
-            /**
-             * Get payment method data
-             */
-            getData: function () {
-                return {
-                    'method': this.getCode(),
-                    'additional_data': {
-                        'ccNumber': this.creditCardNumber(),
-                        'ccMonth': this.creditCardExpMonth(),
-                        'completedTxnId': this.completedTxnId,
-                        'ccYear': this.creditCardExpYear(),
-                        'accordDChoice': this.accordDChoice(),
-                        'accordDType': this.accordDType(),
-                        'accordDGracePeriod': this.accordDGracePeriod(),
-                        'accordDPlanNumber': this.accordDPlanNumber(),
-                    }
-                };
-            },
+          if(parseInt(window.checkoutConfig.payment.paysafe_gateway.threedsecuremode) === 0) {
+            return self.placeOrder();
+          }
+        }
+      },
+
+      initValidation: function () {
+        $('#co-transparent-form-paysafe').on('keyup  paste', 'input, select, textarea', function () {
+          $('#co-transparent-form-paysafe').validation('isValid');
         });
-    }
+      },
+
+      /**
+       * Get payment method data
+       */
+      getData: function () {
+        return {
+          'method': this.getCode(),
+          'additional_data': {
+            'ccNumber': this.creditCardNumber(),
+            'ccMonth': this.creditCardExpMonth(),
+            'completedTxnId': this.completedTxnId,
+            'ccYear': this.creditCardExpYear(),
+            'accordDChoice': this.accordDChoice(),
+            'accordDType': this.accordDType(),
+            'accordDGracePeriod': this.accordDGracePeriod(),
+            'accordDPlanNumber': this.accordDPlanNumber(),
+          }
+        };
+      },
+    });
+  }
 );
