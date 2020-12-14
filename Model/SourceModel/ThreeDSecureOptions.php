0c61ea077b88fec4190d0eb6186edc4c2c240e43
Xavi Nguyen <vphat28@gmail.com>
2020-12-14 23:24:00 +0700

    Remove 3ds v1

--- Model/SourceModel/ThreeDSecureOptions.php
+++ Model/SourceModel/ThreeDSecureOptions.php
@@ -12,8 +12,7 @@ class ThreeDSecureOptions  implements \Magento\Framework\Option\ArrayInterface
     public function toOptionArray()
     {
         return [
-            ['value' => 2, 'label' => __('3DS Version 2')],
-            ['value' => 1, 'label' => __('3DS Version 1')],
+            ['value' => 2, 'label' => __('3DS Version 2')], 
             ['value' => 0, 'label' => __('No')]
         ];
     }
